faa827c1b27c13fd47d17f545920d486b8cc68a9
sakshamgupta912 <saksham.gupta.btech2021@sitpune.edu.in>
2024-03-23 13:53:59 +0530

    Bottom Sheet added for create and enter button

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { Provider } from 'react-native-paper'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { theme } from './src/core/theme'
-
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import AnnouncementScreen from './src/screens/InRoomScreens/AnnouncementScreen'
 import {
   StartScreen,
@@ -17,25 +17,30 @@ const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <Provider theme={theme}>
-      <NavigationContainer>
-        <Stack.Navigator
-          initialRouteName="StartScreen"
-          screenOptions={{
-            headerShown: false,
-          }}
-        >
-          <Stack.Screen name="StartScreen" component={StartScreen} />
-          <Stack.Screen name="LoginScreen" component={LoginScreen} />
-          <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-          <Stack.Screen name="Dashboard" component={Dashboard} />
-          <Stack.Screen name="AnnouncementScreen" component={AnnouncementScreen} />
-          <Stack.Screen
-            name="ResetPasswordScreen"
-            component={ResetPasswordScreen}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </Provider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Provider theme={theme}>
+        <NavigationContainer>
+          <Stack.Navigator
+            initialRouteName="StartScreen"
+            screenOptions={{
+              headerShown: false,
+            }}
+          >
+            <Stack.Screen name="StartScreen" component={StartScreen} />
+            <Stack.Screen name="LoginScreen" component={LoginScreen} />
+            <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+            <Stack.Screen name="Dashboard" component={Dashboard} />
+            <Stack.Screen
+              name="AnnouncementScreen"
+              component={AnnouncementScreen}
+            />
+            <Stack.Screen
+              name="ResetPasswordScreen"
+              component={ResetPasswordScreen}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </Provider>
+    </GestureHandlerRootView>
   )
 }
--- src/screens/DashboardScreens/HomeScreen.js
+++ src/screens/DashboardScreens/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
-import { View, StyleSheet, TouchableOpacity , Text} from 'react-native'
+import { useState, useEffect, useRef } from 'react'
+import { View, StyleSheet, TouchableOpacity, Text } from 'react-native'
 import Icon from 'react-native-ico-material-design'
 import HomeCard from '../../components/HomeCard'
 
@@ -9,26 +9,46 @@ import theme from '../../core/theme'
 import axios from '../../api/axios'
 import AsyncStorage from '@react-native-async-storage/async-storage' // Import AsyncStorage
 import { ActivityIndicator } from 'react-native-paper'
+
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from '@gorhom/bottom-sheet'
+
 const HomeScreen = () => {
-  const [roomCollection, setRoomCollection] = useState(null);
-  const [loading, setLoading] = useState(true); // Manage loading state
-  const [token, setToken] = useState();
-  const [uid, setUID] = useState();
+  const [roomCollection, setRoomCollection] = useState(null)
+  const [loading, setLoading] = useState(true) // Manage loading state
+  const [token, setToken] = useState()
+  const [uid, setUID] = useState()
+
+  const AddPlusBottomSheetModalRef = useRef(null)
+  const EnterBottomSheetModalRef = useRef(null)
+  const snapPoints = ['48%']
+
+  const handleAddPlusButtonPress = () => {
+    AddPlusBottomSheetModalRef.current?.present()
+  }
+  const handleEnterButtonPress = () => {
+    EnterBottomSheetModalRef.current?.present()
+  }
+
+  
+
   async function auth() {
     try {
-      const t = await AsyncStorage.getItem('token');
-      const u = await AsyncStorage.getItem('uid');
-      setToken(t);
-      setUID(u);
+      const t = await AsyncStorage.getItem('token')
+      const u = await AsyncStorage.getItem('uid')
+      setToken(t)
+      setUID(u)
     } catch (e) {
-      console.log('Auth function error: ' + e);
+      console.log('Auth function error: ' + e)
     }
   }
-  
+
   useEffect(() => {
     async function getTeams() {
       try {
-        auth();
+        auth()
         const response = await axios.post(
           '/api/teams/getTeams',
           {},
@@ -40,45 +60,80 @@ const HomeScreen = () => {
             },
             withCredentials: true,
           }
-        );
+        )
 
         if (response.status === 200) {
-          setRoomCollection(response.data);
+          setRoomCollection(response.data)
         } else {
-          console.error('Error fetching room data:', response.statusText);
+          console.error('Error fetching room data:', response.statusText)
         }
       } catch (error) {
-        console.error('Error fetching room data:', error);
+        console.error('Error fetching room data:', error)
       } finally {
-        setLoading(false); // Set loading to false when data fetching is complete
+        setLoading(false) // Set loading to false when data fetching is complete
       }
     }
 
-    getTeams();
-  }, [uid,token]); 
+    getTeams()
+  }, [uid, token])
 
-  console.log(roomCollection);
+  console.log(roomCollection)
 
   return (
-    <View style={styles.container}>
-    
-       <ScrollView>
-        {loading ? ( // Render loading indicator while loading
-          <ActivityIndicator size="large" color={theme.colors.primary} />
-        ) : Array.isArray(roomCollection) && roomCollection.length > 0 ? (
-          roomCollection.map((room) => <HomeCard key={room._id} room={room} />)
-        ) : (
-          <></>
-        )}
-      </ScrollView>
-      {/* Floating button */}
-      <TouchableOpacity style={styles.createButton}>
-        <Icon name="add-plus-button" size={30} color="#fff" />
-      </TouchableOpacity>
-      <TouchableOpacity style={styles.enterButton}>
-        <Icon name="exit-to-app-button" size={30} color="#fff" />
-      </TouchableOpacity>
-    </View>
+    <BottomSheetModalProvider>
+      <View style={styles.container}>
+        <ScrollView>
+          {loading ? ( // Render loading indicator while loading
+            <ActivityIndicator size="large" color={theme.colors.primary} />
+          ) : Array.isArray(roomCollection) && roomCollection.length > 0 ? (
+            roomCollection.map((room) => (
+              <HomeCard key={room._id} room={room} />
+            ))
+          ) : (
+            <></>
+          )}
+        </ScrollView>
+        {/* Floating button */}
+        <TouchableOpacity
+          style={styles.createButton}
+          onPress={handleAddPlusButtonPress}
+        >
+          <Icon name="add-plus-button" size={30} color="#fff" />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.enterButton}
+          onPress={handleEnterButtonPress}
+        >
+          <Icon name="exit-to-app-button" size={30} color="#fff" />
+        </TouchableOpacity>
+
+        {/* Add Plus Bottom Sheet */}
+        <BottomSheetModal
+          style={styles.bottomSheet}
+          backgroundStyle={{ borderRadius: 40 }}
+          ref={AddPlusBottomSheetModalRef}
+          index={0}
+          snapPoints={snapPoints}
+        >
+          <View>
+            <Text>Hello Add Plus Bottom Sheet</Text>
+          </View>
+        </BottomSheetModal>
+
+        {/* Enter Bottom Sheet */}
+        <BottomSheetModal
+          style={styles.bottomSheet}
+          backgroundStyle={{ borderRadius: 40 }}
+          ref={EnterBottomSheetModalRef}
+          index={0}
+          snapPoints={snapPoints}
+        >
+          <View>
+            <Text>Hello Enter Bottom Sheet</Text>
+          </View>
+        </BottomSheetModal>
+      </View>
+    </BottomSheetModalProvider>
   )
 }
 
@@ -111,7 +166,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     elevation: 3, // Add elevation for Android shadow effect
   },
- 
+  bottomSheet: {
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+  },
 })
 
 export default HomeScreen
