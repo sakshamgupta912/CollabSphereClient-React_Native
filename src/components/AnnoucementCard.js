51196ee36d9e2629a4859790d1f4547f532fa094
Savar Srivastava <savarsri@gmail.com>
2024-04-14 18:47:50 +0530

    Added download functionality

--- src/components/AnnoucementCard.js
+++ src/components/AnnoucementCard.js
@@ -1,85 +1,150 @@
-import React,{useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-ico-material-design'
-import * as FileSystem from 'expo-file-system'
-import * as Sharing from 'expo-sharing'
+import { shareAsync } from 'expo-sharing'
+import { Platform } from 'react-native'
 import axios from '../api/axios'
 import theme from '../core/theme'
+import * as MediaLibrary from 'expo-media-library'
+import * as FileSystem from 'expo-file-system'
+import * as Permissions from 'expo-permissions'
 
-const AnnoucementCard = ({ post, uid, token ,isAdmin,setUpdate}) => {
+const AnnoucementCard = ({ post, uid, token, isAdmin, setUpdate }) => {
   const [deleteEnable, setDeleteEnable] = useState(false)
-  
-  const handleDelete = async () => {
-    try{
-        const response = await axios.post(
-            `/api/post/deletePost`,
-            JSON.stringify({ postID: post._id }),
-            {
-              headers: {
-                "Content-Type": "application/json",
-                authorization: `Token ${token}`,
-                uid,
-              },
-              validateStatus: () => true
-            }
-          );
-          setUpdate((prevState)=>(prevState+1));
 
+  const handleDelete = async () => {
+    try {
+      const response = await axios.post(
+        `/api/post/deletePost`,
+        JSON.stringify({ postID: post._id }),
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            authorization: `Token ${token}`,
+            uid,
+          },
+          validateStatus: () => true,
+        }
+      )
+      setUpdate((prevState) => prevState + 1)
+    } catch (error) {
+      alert('Error in Deletion of chat..')
+      console.error('Error fetching deleting from API:', error)
     }
-    catch(error)
-    {
-        alert('Error in Deletion of chat..');
-        console.error('Error fetching deleting from API:', error)
-    }
-    
-  };
+  }
 
   useEffect(() => {
     if (uid === post.createdBy._id || isAdmin) {
-      setDeleteEnable(true);
+      setDeleteEnable(true)
     }
-  }, []);
+  }, [])
 
-  const handleDownload = async (file) => {
-    const fileId = file._id
+  // const handleDownload = async (file) => {
+  //   const fileId = file._id
+  //   try {
+  //     const response = await axios.get(`/api/download/`, {
+  //       responseType: 'blob',
+  //       headers: {
+  //         Authorization: `Token ${token}`,
+  //         fileid: fileId,
+  //       },
+  //     })
+
+  //     if (response.status === 200) {
+  //       const blobId = response.data.blobId
+
+  //       if (blobId) {
+  //         const fileName = `file_${blobId}.png` // Assuming the file is a PDF, adjust the extension as needed
+  //         const downloadDest = `${FileSystem.documentDirectory}${fileName}`
+
+  //         try {
+  //           const downloadObject = FileSystem.createDownloadResumable(
+  //             blobId,
+  //             downloadDest
+  //           )
+  //           const { uri: localUri } = await downloadObject.downloadAsync()
+  //           console.log('File downloaded successfully:', localUri)
+  //           await Sharing.shareAsync(localUri)
+  //         } catch (error) {
+  //           console.error('Error downloading file:', error)
+  //         }
+  //       } else {
+  //         console.error('Invalid API response: blobId is undefined')
+  //       }
+  //     } else if (response.status === 304) {
+  //       console.log('File has not been modified since the last request.')
+  //       // You can handle the 304 status code here, for example, by showing a message to the user or retrieving the file from cache if available.
+  //     } else {
+  //       console.error('Error fetching file from API:', response.status)
+  //     }
+  //   } catch (error) {
+  //     console.error('Error fetching file from API:', error)
+  //   }
+  // }
+
+  const downloadFile = async (file) => {
     try {
-      const response = await axios.get(`/api/download/`, {
-        responseType: 'blob',
-        headers: {
-          Authorization: `Token ${token}`,
-          fileid: fileId,
-        },
-      })
+      // Check and request permissions if necessary
+      // if (Platform.OS === 'android') {
+      //   const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL)
+      //   if (status !== 'granted') {
+      //     console.log('Camera roll permission denied')
+      //     return
+      //   }
+      // }
+      // Construct the download URL
+      const downloadUrl = 'http://192.168.1.3:3000/api/download'
 
-      if (response.status === 200) {
-        const blobId = response.data.blobId
+      // Construct the headers with fileId
+      const headers = {
+        fileid: file._id,
+      }
 
-        if (blobId) {
-          const fileName = `file_${blobId}.png` // Assuming the file is a PDF, adjust the extension as needed
-          const downloadDest = `${FileSystem.documentDirectory}${fileName}`
-
-          try {
-            const downloadObject = FileSystem.createDownloadResumable(
-              blobId,
-              downloadDest
-            )
-            const { uri: localUri } = await downloadObject.downloadAsync()
-            console.log('File downloaded successfully:', localUri)
-            await Sharing.shareAsync(localUri)
-          } catch (error) {
-            console.error('Error downloading file:', error)
-          }
-        } else {
-          console.error('Invalid API response: blobId is undefined')
+      // Send a request to download the file with headers
+      const downloadResult = await FileSystem.downloadAsync(
+        downloadUrl,
+        FileSystem.documentDirectory + file.originalname, // Save the file in the documents directory
+        {
+          headers: headers,
         }
-      } else if (response.status === 304) {
-        console.log('File has not been modified since the last request.')
-        // You can handle the 304 status code here, for example, by showing a message to the user or retrieving the file from cache if available.
+      )
+
+      if (downloadResult.status === 200) {
+        console.log('File downloaded successfully:', downloadResult.uri)
+        save(downloadResult.uri, file.originalname,  downloadResult.headers["Content-Type"])
+        // Now you can use the downloaded file URI for further processing
       } else {
-        console.error('Error fetching file from API:', response.status)
+        console.log('Failed to download file')
       }
     } catch (error) {
-      console.error('Error fetching file from API:', error)
+      console.error('Error downloading file:', error)
+    }
+  }
+
+  const save = async (uri, filename, mimetype) => {
+    if (Platform.OS === 'android') {
+      const permissions =
+        await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync()
+      if (permissions.granted) {
+        const base64 = await FileSystem.readAsStringAsync(uri, {
+          encoding: FileSystem.EncodingType.Base64,
+        })
+        await FileSystem.StorageAccessFramework.createFileAsync(
+          permissions.directoryUri,
+          filename,
+          mimetype
+        )
+          .then(async (uri) => {
+            await FileSystem.writeAsStringAsync(uri, base64, {
+              encoding: FileSystem.EncodingType.Base64,
+            })
+          })
+          .catch((e) => console.log(e))
+      } else {
+        shareAsync(uri)
+      }
+    } else {
+      shareAsync(uri)
     }
   }
 
@@ -106,7 +171,8 @@ const AnnoucementCard = ({ post, uid, token ,isAdmin,setUpdate}) => {
         <View style={styles.fileContainer}>
           <Text style={styles.fileLabel}>Files:</Text>
           {post.files.map((file, index) => (
-            <TouchableOpacity key={index} onPress={() => handleDownload(file)}>
+            // <TouchableOpacity key={index} onPress={() => handleDownload(file)}>
+            <TouchableOpacity key={index} onPress={() => downloadFile(file)}>
               <View style={styles.fileItemContainer}>
                 <Icon name="attachment-clip" color="#000" />
                 <Text style={styles.fileItem}>{file.originalname}</Text>
