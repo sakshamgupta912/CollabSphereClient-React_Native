174beb65de8f571f5634a34d73efd686e8c49c9c
sakshamgupta912 <saksham.gupta.btech2021@sitpune.edu.in>
2024-04-17 00:28:27 +0530

    minr change

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 // const baseURL = 'http://192.168.1.7:3000'
-const baseURL= 'https://collabspheresever.azurewebsites.net/'
+const baseURL= 'https://collabspheresever.azurewebsites.net'
 export default axios.create({
-  baseURL: 'https://collabspheresever.azurewebsites.net/',
+  baseURL: 'https://collabspheresever.azurewebsites.net',
   // baseURL: 'http://192.168.1.7:3000',
 })
 
